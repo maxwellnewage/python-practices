55e8e99b58b98930163f6e2f7969891a1748e1cc
Maximiliano Burgos <maxib2007@gmail.com>
2022-10-22 14:10:12 -0300

    wip para trabajos en progreso

---
+++ wip.py
@@ -0,0 +1,6 @@
+"""
+¡Lo siento! Si llegaste a este archivo,
+significa que todavía no publiqué el script correspondiente
+WIP significa Work in Progress (Trabajo en Progreso),
+por lo cual, pronto vas a tener novedades, ¡paciencia! :D
+"""
