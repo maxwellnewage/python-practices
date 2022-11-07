63460ab46f5f32eef2780068efd1a067d343844c
Maximiliano Burgos <maxib2007@gmail.com>
2022-11-07 11:22:53 -0300

    fix kwargs.py para multiples resultados

--- level_beginner/kwargs.py
+++ level_beginner/kwargs.py
@@ -4,6 +4,7 @@ Crear un filtro que busque personas en un diccionario
 
 persons = [
     {"first_name": "Max", "last_name": "Thompson", "age": 32},
+    {"first_name": "Max", "last_name": "Hilton", "age": 35},
     {"first_name": "Rocky", "last_name": "Clans"},
     {"first_name": "Linda", "last_name": "Dimmer", "job": "Developer"},
 ]
@@ -12,7 +13,7 @@ persons = [
 def search_person(**attrs):
     person_list = []
     for key, value in attrs.items():
-        person_list = [person for person in persons if key in person and value == person[key]]
+        person_list += [person for person in persons if key in person and value == person[key]]
     return person_list
 
 
