9aeaa6f174991c68a0185db0017d21bf69e664b7
Maximiliano Burgos <maxib2007@gmail.com>
2022-11-19 13:06:11 -0300

    fix numpy memory sizes

--- level_beginner/numpy_practices.py
+++ level_beginner/numpy_practices.py
@@ -1,18 +1,20 @@
 """
 Prácticas en numpy
 """
-import array
+
 from sys import getsizeof
 import numpy as np
 
 
 print(f"Tipo: {type(10)} | Tamaño: {getsizeof(10)} bytes")
 
-print(f"Tipo: {type(np.int8(10))} | Tamaño: {getsizeof(np.int8(10))} bytes")
+np_int = np.int8(10)
+print(f"Tipo: {np.dtype(np_int)} | Tamaño: {np.dtype(np_int).itemsize} bytes")
 
-print(f"Tipo: {type(array.array('i', [1, 2, 3]))} | Tamaño: {getsizeof([1, 2, 3])} bytes")
+print(f"Tipo: {type([1, 2, 3])} | Tamaño: {getsizeof([1, 2, 3])} bytes")
 
-print(f"Tipo: {type(np.array([1, 2, 3]))} | Tamaño: {getsizeof(np.array([1, 2, 3]))} bytes")
+np_array = np.array([1, 2, 3])
+print(f"Tipo: {np_array.dtype} | Tamaño: {np_array.nbytes} bytes")
 
 py_multi_list = [
     [1, 2, 3],
@@ -25,4 +27,4 @@ np_multi_array = np.array([
 ])
 
 print(f"Tipo: {type(py_multi_list)} | Tamaño: {getsizeof(py_multi_list)} bytes")
-print(f"Tipo: {type(np_multi_array)} | Tamaño: {getsizeof(np_multi_array)} bytes")
+print(f"Tipo: {np_multi_array.dtype} | Tamaño: {np_multi_array.nbytes} bytes")
