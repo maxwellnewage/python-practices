5f22342c6680111278c0c1342ed880bcec396041
Maximiliano Burgos <maxib2007@gmail.com>
2023-05-03 12:00:32 -0300

    se mejora el ejemplo de find_vowels con ignore case

--- level_advance/regex/vowel_detector.py
+++ level_advance/regex/vowel_detector.py
@@ -5,10 +5,15 @@ import re
 
 
 def find_vowels(phrase):
-    vowel_regex = re.compile(r'[aeiouAEIOU]')
+    vowel_regex = re.compile(r'[aeiou]')
     return vowel_regex.findall(phrase)
 
 
+def find_vowels_ignore_case(phrase):
+    return re.compile(r'[aeiou]', re.IGNORECASE).findall(phrase)
+
+
 if __name__ == '__main__':
-    print(find_vowels('Quiero las vocales de esta cadena'))
+    print(find_vowels('Quiero las vOcalEs de Esta cadena'))
+    print(find_vowels_ignore_case('Quiero las vOcalEs de Esta cadenA'))
 
