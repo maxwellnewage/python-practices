8c0a1781d098902dc6f17079fec644114d228585
Maximiliano Burgos <maxib2007@gmail.com>
2023-05-03 12:19:03 -0300

    version VERBOSE para phone number finder

--- level_advance/regex/phone_number_finder.py
+++ level_advance/regex/phone_number_finder.py
@@ -9,9 +9,21 @@ def find_all_phone_numbers(text):
     return phone_regex.findall(text)
 
 
+def find_all_phone_numbers_verbose(text):
+    return re.compile(r'''
+        \d\d\d
+        -
+        \d\d\d
+        -
+        \d\d\d\d''', re.VERBOSE).findall(text)
+
+
 if __name__ == '__main__':
-    print(find_all_phone_numbers("""
+    text = """
         Esto es un numero de télefono: 415-232-2333, 
         esto no 444-222, 
         pero esto si 445-252-2333
-    """))
+    """
+
+    print(find_all_phone_numbers(text))
+    print(find_all_phone_numbers_verbose(text))
