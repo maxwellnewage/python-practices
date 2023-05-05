ad29abbea23ab0e914e09c6bd95974b34e892190
Maximiliano Burgos <maxib2007@gmail.com>
2023-05-05 12:08:09 -0300

    se agregó un ejemplo más al decorador

--- level_advance/decorator.py
+++ level_advance/decorator.py
@@ -16,5 +16,11 @@ def sum_2_nums(n1, n2):
     print(f"La suma de {n1} y {n2} es {n1 + n2}")
 
 
+@calculate
+def sum_3_nums(n1, n2, n3):
+    print(f"{n1} + {n2} + {n3} = {n1 + n2 + n3}")
+
+
 if __name__ == '__main__':
     sum_2_nums(1, 2)
+    sum_3_nums(1, 2, 3)
