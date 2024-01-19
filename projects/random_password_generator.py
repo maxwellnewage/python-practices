17f8a42afe4c38d05fa90d740aa91f160dec4636
Maximiliano Burgos <maxib2007@gmail.com>
2024-01-19 20:14:06 -0300

    random password generator refactor para poder llamarla desde otros scripts

--- projects/random_password_generator.py
+++ projects/random_password_generator.py
@@ -11,11 +11,9 @@ def set_char_upper_or_lower(char: str):
     return char.upper() if choice == 0 else char.lower()
 
 
-def generate_pw():
+def generate_pw(pw_len=10, pw_count=1):
     chars = "abcdefghijklmnopqrstuvwxyz123456!$%^(*)"
     password_list = []
-    pw_len = int(input("Que tamaño de contraseña quieres?: "))
-    pw_count = int(input("Cuantas contraseñas te gustaría generar?: "))
 
     for _ in range(0, pw_count):
         password = ""
@@ -26,6 +24,12 @@ def generate_pw():
     return password_list
 
 
-pw_list = generate_pw()
-for i, pw in enumerate(pw_list):
-    print(f"Password #{i+1}: {pw}")
+if __name__ == '__main__':
+    input_pw_len = int(input("Que tamaño de contraseña quieres?: "))
+    input_pw_count = int(input("Cuantas contraseñas te gustaría generar?: "))
+
+    pw_list = generate_pw(input_pw_len, input_pw_count)
+    for i, pw in enumerate(pw_list):
+        print(f"Password #{i + 1}: {pw}")
+
+    print(generate_pw()[0])
