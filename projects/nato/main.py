df6938147c788860bb300fedfdb4aa9412686cc0
Maximiliano Burgos <maxib2007@gmail.com>
2024-01-19 21:19:56 -0300

    refactor nato project

--- projects/nato/main.py
+++ projects/nato/main.py
@@ -6,8 +6,13 @@ import pandas as pd
 
 data = pd.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+while True:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+    else:
+        print(output_list)
+        break
