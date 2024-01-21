aa592a477de673dc9edc2330564ffb4872bb932c
Maximiliano Burgos <maxib2007@gmail.com>
2024-01-21 15:30:25 -0300

    fixed email sender

--- projects/email_sender/main.py
+++ projects/email_sender/main.py
@@ -5,14 +5,22 @@ https://www.youtube.com/watch?v=g_j6ILT-X0k
 
 import smtplib
 import ssl
-from decouple import config
+import os
 from email.message import EmailMessage
+from dotenv import load_dotenv
 
+# Cargar variables de entorno desde el archivo .env en el mismo directorio
+load_dotenv()
+
+# constantes
+SENDER_EMAIL = os.getenv('SENDER_EMAIL')
+RECEIVER_EMAIL = os.getenv('RECEIVER_EMAIL')
+SENDER_TOKEN = os.getenv('SENDER_TOKEN')
 
 # armo el mensaje del email
 email_message = EmailMessage()
-email_message['From'] = config('SENDER_EMAIL')
-email_message['To'] = config('RECIEVER_EMAIL')
+email_message['From'] = SENDER_EMAIL
+email_message['To'] = RECEIVER_EMAIL
 email_message['Subject'] = "hola!"
 email_message.set_content("""
 Hola Maxi!
@@ -30,13 +38,13 @@ print("conectando...")
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
     # me autentico
     server.login(
-        config('SENDER_EMAIL'),
-        config('SENDER_TOKEN')
+        SENDER_EMAIL,
+        SENDER_TOKEN
     )
     # envío el email
     server.sendmail(
-        config('SENDER_EMAIL'),
-        config('RECIEVER_EMAIL'),
+        SENDER_EMAIL,
+        RECEIVER_EMAIL,
         email_message.as_string()
     )
 
