b0b95afcee3b52ca478e99bfcb8322651e2fd904
Maximiliano Burgos <maxib2007@gmail.com>
2024-01-26 11:51:03 -0300

    Se desacopla config de selenium para mejorar escalabilidad

---
+++ projects/config/selenium.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from projects.config.globals import CHROMIUM_BINARY_PATH
+
+__options = Options()
+__options.add_experimental_option("detach", True)
+
+# especifica un directorio de la instalación de Chrome
+__options.binary_location = CHROMIUM_BINARY_PATH
+
+# inicializo el driver de Chrome
+driver = webdriver.Chrome(
+    options=__options
+)
--- projects/selenium_app/main.py
+++ projects/selenium_app/main.py
@@ -4,23 +4,10 @@ https://youtu.be/SPM1tm2ZdK4
 """
 
 import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from projects.config.globals import CHROMIUM_BINARY_PATH
+from projects.config.selenium import driver
 
 if __name__ == '__main__':
-    options = Options()
-    options.add_experimental_option("detach", True)
-
-    # especifica un directorio de la instalación de Chrome
-    options.binary_location = CHROMIUM_BINARY_PATH
-
-    # inicializo el driver de Chrome
-    driver = webdriver.Chrome(
-        options=options
-    )
-
     # accedo a una URL
     driver.get("https://www.neuralnine.com/")
 
