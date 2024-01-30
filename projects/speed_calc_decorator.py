7b569ceb16feff94d96f965e5b475b987c2135e9
Maximiliano Burgos <maxib2007@gmail.com>
2024-01-29 23:03:09 -0300

    Speed Calculator Decorator mejorada

--- projects/speed_calc_decorator.py
+++ projects/speed_calc_decorator.py
@@ -7,28 +7,28 @@ from bad_word_detector import phrase_filter
 
 
 def speed_calculator(function):
-    def wrapper_function():
+    def wrapper_function(*args):
         start_time = t.time()
-        function()
+        function(*args)
         end_time = t.time()
 
-        print(f"{function.__name__} | run speed: {end_time - start_time}")
+        print(f"{function.__name__} | run speed: {end_time - start_time} | called {args[0]} times")
 
     return wrapper_function
 
 
 @speed_calculator
-def short_phrase():
-    for _ in range(100*100):
+def short_phrase(times: int):
+    for _ in range(times):
         phrase_filter("Me gusta la manzana")
 
 
 @speed_calculator
-def long_phrase():
-    for _ in range(100*100):
+def long_phrase(times: int):
+    for _ in range(times):
         phrase_filter("Me gusta la manzana, además de la pera y la banana")
 
 
 if __name__ == '__main__':
-    short_phrase()
-    long_phrase()
+    short_phrase(100*1000)
+    long_phrase(100*100)
