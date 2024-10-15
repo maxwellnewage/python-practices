f9f0c4120adac26830db110eed98e3348a5af290
Maximiliano Burgos <maxib2007@gmail.com>
2024-10-15 20:50:39 -0300

    anime finder: main for run script

--- projects/anime_finder.py
+++ projects/anime_finder.py
@@ -17,35 +17,36 @@ def search_anime_by_term(term):
     ).json()
 
 
-print("--------------------------------------------")
-print("Bienvenido a Anime Finder!")
-input_term = input("Dime que anime quieres buscar: ")
-print("Buscando...")
-print("--------------------------------------------")
+if __name__ == '__main__':
+    print("--------------------------------------------")
+    print("Bienvenido a Anime Finder!")
+    input_term = input("Dime que anime quieres buscar: ")
+    print("Buscando...")
+    print("--------------------------------------------")
 
-anime_list: list = search_anime_by_term(input_term)['data']
-if not anime_list:
-    print("No se encontraron resultados :(")
-else:
-    for anime in anime_list:
-        print("Animes encontrados:")
-        print("Títulos del anime")
-        for item in anime['titles']:
-            print(f"Idioma {item['type']}: {item['title']}")
-        print(f"Tipo: {anime['type']}")
-        print(f"Episodios: {anime['episodes']}")
-        print(f"Estado Actual: {anime['status']}")
-        print(f"Duración: {anime['duration']}")
-        print(f"Rating: {anime['rating']}")
-        print(f"Score: {anime['score']}")
-        print(f"Popularidad: {anime['popularity']}")
-        season = anime['season'] \
-            if anime['season'] is not None \
-            else 'No existe información'
-        print(f"Temporada: {season}")
+    anime_list: list = search_anime_by_term(input_term)['data']
+    if not anime_list:
+        print("No se encontraron resultados :(")
+    else:
+        for anime in anime_list:
+            print("Animes encontrados:")
+            print("Títulos del anime")
+            for item in anime['titles']:
+                print(f"Idioma {item['type']}: {item['title']}")
+            print(f"Tipo: {anime['type']}")
+            print(f"Episodios: {anime['episodes']}")
+            print(f"Estado Actual: {anime['status']}")
+            print(f"Duración: {anime['duration']}")
+            print(f"Rating: {anime['rating']}")
+            print(f"Score: {anime['score']}")
+            print(f"Popularidad: {anime['popularity']}")
+            season = anime['season'] \
+                if anime['season'] is not None \
+                else 'No existe información'
+            print(f"Temporada: {season}")
 
-        year = anime['year'] \
-            if anime['year'] is not None \
-            else 'No existe información'
-        print(f"Año: {year}")
-        print("---------------------------------")
+            year = anime['year'] \
+                if anime['year'] is not None \
+                else 'No existe información'
+            print(f"Año: {year}")
+            print("---------------------------------")
